34a13e19d6a0b3fbaae3d672e80939ab69b6b6f8
Julliet Kgomotso Rakuba <jullietrakuba@gmail.com>
2025-02-08 09:18:25 +0200

    Uploaded files

---
+++ IncomeAndExpenses/IncomeAndExpenses/Form1.cs
@@ -0,0 +1,25 @@
+namespace IncomeAndExpenses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Do you want to buy a vehicle? Yes/No")
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
